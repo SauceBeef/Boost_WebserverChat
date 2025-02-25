0bf208e0fd4978036091553c71006c0c806c62b8
unknown <1732983158@qq.com>
2025-02-25 21:57:33 +0800

    Init

---
+++ client/components/Head.tsx
@@ -0,0 +1,10 @@
+import NextHead from "next/head";
+
+export default function Head() {
+  return (
+    <NextHead>
+      <title>程序员老廖 聊天室</title>
+      <link rel="icon" href="/favicon.ico" />
+    </NextHead>
+  );
+}
